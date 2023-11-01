6404fa95447df2823c7f58ffdf1cc39e07b753a5
Koushik Debnath <111985459+DevNinjaX@users.noreply.github.com>
2023-11-01 09:21:05 +0530

    linearSearch.c

--- Search/linearSearch.c
+++ Search/linearSearch.c
@@ -1,3 +1,15 @@
+/**
+Algorithm
+--------------------------------------
+procedure linear_search (list, value)
+   for each item in the list
+      if match item == value
+         return the item's location
+      end if
+   end for
+end procedure
+**/
+
 // linear search
 #include <stdio.h>
 #include <conio.h>
